a0b7a057261fcdd7928963dee6b295ba92da8434
Thành Trần Tấn <trantanthanh@gmail.com>
2024-04-11 15:38:43 +0700

    check jump state animation, use Physic2D.RayCast to check hit with ground mask layer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 3f;
     [SerializeField] float jumpForce = 5f;
+    [SerializeField] float groundCheckDistance;
+    [SerializeField] LayerMask groundMask;
+
+    float timeBetweenJump = 0.3f;
+    float timeNextJump = 0f;
+    bool isGrounded = false;
+
     Rigidbody2D myRigid;
     Animator myAnimator;
 
@@ -30,25 +38,57 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckIsOnGround();
         float inputHorizontal = Input.GetAxis("Horizontal");
         myRigid.velocity = new Vector2(inputHorizontal * moveSpeed, myRigid.velocity.y);
         if (myRigid.velocity.x != 0)
         {
-            SetState(PlayerState.RUN);
+            if (isGrounded)
+            {
+                SetState(PlayerState.RUN);
+            }
             CheckFlipSprite();
         }
         else
         {
-            SetState(PlayerState.IDLE);
+            if (isGrounded && !IsState(PlayerState.JUMP))
+            {
+                SetState(PlayerState.IDLE);
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && CanJump())
         {
+            timeNextJump = Time.time + timeBetweenJump;
+            isGrounded = false;
             SetState(PlayerState.JUMP);
             myRigid.velocity = new Vector2(myRigid.velocity.x, jumpForce);
         }
+
+        UpdateState();
     }
 
+    bool CanJump()
+    {
+        return Time.time > timeNextJump;
+    }
+
+    void CheckIsOnGround()
+    {
+        if (IsState(PlayerState.JUMP))
+        {
+            if (!CanJump()) return;
+        }
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundMask);
+        if (hitInfo.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
     void CheckFlipSprite()
     {
         transform.localScale = new Vector2(Mathf.Sign(myRigid.velocity.x) * Mathf.Abs(transform.localScale.x), transform.localScale.y);
@@ -71,6 +111,7 @@ public class Player : MonoBehaviour
                 }
             case PlayerState.JUMP:
                 {
+                    myAnimator.SetBool("isMoving", false);
                     myAnimator.SetTrigger("Jump");
                     break;
                 }
@@ -84,5 +125,47 @@ public class Player : MonoBehaviour
                     break;
                 }
         }
+    }
+
+    bool IsState(PlayerState state)
+    {
+        return playerSate == state;
+    }
+
+    void UpdateState()
+    {
+        switch (playerSate)
+        {
+            case PlayerState.NONE:
+                {
+                    break;
+                }
+            case PlayerState.IDLE:
+                {
+                    break;
+                }
+            case PlayerState.JUMP:
+                {
+                    if (isGrounded && myRigid.velocity.x == 0)
+                    {
+                        myAnimator.SetTrigger("Landed");
+                        SetState(PlayerState.IDLE);
+                    }
+                    break;
+                }
+            case PlayerState.RUN:
+                {
+                    break;
+                }
+            case PlayerState.ATTACK:
+                {
+                    break;
+                }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
