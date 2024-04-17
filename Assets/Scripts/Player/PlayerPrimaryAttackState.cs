9f4be4303c255466690852548fc91ab8793ab9b2
Thành Trần Tấn <trantanthanh@gmail.com>
2024-04-17 15:51:00 +0700

    combo attack

--- Assets/Scripts/Player/PlayerPrimaryAttackState.cs
+++ Assets/Scripts/Player/PlayerPrimaryAttackState.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerPrimaryAttackState : PlayerState
 {
+    private int comboCounter = 0;
+    private int numOfCombo = 3;
+    private float timeCombo= 1f;//next attack must below this value to increase comboCounter;
+    private float timeNextAttack = 0f;
+
     public PlayerPrimaryAttackState(Player _player, PlayerSateMachine _stateMachine, string _animName) : base(_player, _stateMachine, _animName)
     {
     }
@@ -12,6 +17,15 @@ public class PlayerPrimaryAttackState : PlayerState
     {
         base.Enter();
         player.SetVelocity(0, 0);
+
+        comboCounter %= numOfCombo;
+        if (Time.time > timeNextAttack)
+        {
+            //Over time to do combo
+            comboCounter = 0;
+        }
+        timeNextAttack = Time.time + timeCombo;
+        player.animator.SetInteger("ComboCounter", comboCounter);
     }
 
     public override void Exit()
@@ -24,6 +38,7 @@ public class PlayerPrimaryAttackState : PlayerState
         base.Update();
         if (triggerCalled)
         {
+            comboCounter++;
             stateMachine.ChangeState(player.idleState);
         }
     }
