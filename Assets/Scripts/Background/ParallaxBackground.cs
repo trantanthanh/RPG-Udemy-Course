f0a8b84f2af292e323d75829583e5b45b38d114a
Thành Trần Tấn <trantanthanh@gmail.com>
2024-04-23 13:57:10 +0700

    endless rolling background

--- Assets/Scripts/Background/ParallaxBackground.cs
+++ Assets/Scripts/Background/ParallaxBackground.cs
@@ -7,17 +7,29 @@ public class ParallaxBackground : MonoBehaviour
     [SerializeField] float parallaxEffect;
     private GameObject cam;
     private float xPosition;
+    private float length;
     // Start is called before the first frame update
     void Start()
     {
         cam = GameObject.Find("Main Camera");
         xPosition = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+        float distanceMoved = cam.transform.position.x * (1 - parallaxEffect);
         float distanceToMove = cam.transform.position.x * parallaxEffect;
         transform.position = new Vector3(xPosition + distanceToMove, transform.position.y);
+
+        if (distanceMoved > xPosition + length)//check moved right
+        {
+            xPosition = xPosition + length;
+        }
+        else if (distanceMoved < xPosition - length)//check moved left
+        {
+            xPosition -= length;
+        }
     }
 }
