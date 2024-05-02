b50b95552d55f723356500ad7e14f4f80e0cd8ad
Thành Trần Tấn <trantanthanh@gmail.com>
2024-05-02 22:03:48 +0700

    make the sword collider and stick with object collider

--- Assets/Scripts/Skills/SwordSkillController.cs
+++ Assets/Scripts/Skills/SwordSkillController.cs
@@ -8,6 +8,8 @@ public class SwordSkillController : MonoBehaviour
     private Rigidbody2D rb;
     private CircleCollider2D circleCollider;
 
+    private bool canRotate;
+
     private void Awake()
     {
         animator = GetComponentInChildren<Animator>();
@@ -17,12 +19,27 @@ public class SwordSkillController : MonoBehaviour
 
     public void SetupSword(Vector2 _dir, float _gravityScale)
     {
+        canRotate = true;
         rb.velocity = _dir;
         rb.gravityScale = _gravityScale;
     }
 
     private void Update()
     {
-        transform.right = rb.velocity;
+        if (canRotate)
+        {
+            transform.right = rb.velocity;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        canRotate = false;
+        circleCollider.enabled = false;
+        
+        rb.isKinematic = true;
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+
+        transform.parent = collision.transform;
     }
 }
