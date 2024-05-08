f903f99d369f69dcfcd246e808585defbdf45cca
Thành Trần Tấn <trantanthanh@gmail.com>
2024-05-09 01:31:12 +0700

    update crystal explode animation

--- Assets/Scripts/Skills/CrystalSkill.cs
+++ Assets/Scripts/Skills/CrystalSkill.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CrystalSkill : Skill
 {
     [SerializeField] GameObject crystalPrefab;
+    [SerializeField] float crystalDuration = 5f;
     private GameObject currentCrystal;
     public override bool CanUseSkill()
     {
@@ -18,12 +19,15 @@ public class CrystalSkill : Skill
         if (currentCrystal == null)
         {
             currentCrystal = Instantiate(crystalPrefab, player.transform.position, Quaternion.identity);
+            currentCrystal.GetComponent<CrystalSkillController>().SetupCrystal(crystalDuration);
         }
         else
         {
-            player.transform.position = currentCrystal.transform.position;//teleport to crystal
-            currentCrystal.GetComponent<CrystalSkillController>().Explode();
-            Destroy(currentCrystal, 2f);
+            if (!currentCrystal.GetComponent<CrystalSkillController>().isDestroying)
+            {
+                player.transform.position = currentCrystal.transform.position;//teleport to crystal
+                currentCrystal.GetComponent<CrystalSkillController>().Explode();
+            }
         }
     }
 
--- Assets/Scripts/Skills/SkillController/CrystalSkillController.cs
+++ Assets/Scripts/Skills/SkillController/CrystalSkillController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CrystalSkillController : MonoBehaviour
 {
     private Animator animator;
+    private float crystalExistTimer;
+    public bool isDestroying { get; private set; } = false;
     // Start is called before the first frame update
     void Awake()
     {
@@ -13,6 +16,23 @@ public class CrystalSkillController : MonoBehaviour
 
     public void Explode()
     {
+        isDestroying = true;
         animator.SetBool("Explode", true);
+        Destroy(gameObject, animator.GetCurrentAnimatorStateInfo(0).length);
+    }
+
+    public void SetupCrystal(float _crystalDuration)
+    {
+        this.crystalExistTimer = _crystalDuration;
+        isDestroying = false;
+    }
+
+    private void Update()
+    {
+        crystalExistTimer -= Time.deltaTime;
+        if (crystalExistTimer < 0 && !isDestroying)
+        {
+            Explode();
+        }
     }
 }
