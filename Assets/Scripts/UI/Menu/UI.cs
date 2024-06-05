44af5dc1f1ce530ec60c4f1f2b8b2c9c38a397f4
Thành Trần Tấn <trantanthanh@gmail.com>
2024-06-05 13:06:02 +0700

    add menu UI

---
+++ Assets/Scripts/UI/Menu/UI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public void SwitchMenuTo(GameObject _menu)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+        if (_menu != null)
+        {
+            _menu.SetActive(true);
+        }
+    }
+}
