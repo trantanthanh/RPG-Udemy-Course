7425126923a9cdc4e526f832efae037264258cb8
Thành Trần Tấn <trantanthanh@gmail.com>
2024-06-08 02:21:57 +0700

    init craft window

--- Assets/Scripts/UI/UI_CraftList.cs
+++ Assets/Scripts/UI/UI_CraftList.cs
@@ -10,48 +10,44 @@ public class UI_CraftList : MonoBehaviour, IPointerDownHandler
     [SerializeField] private GameObject craftSlotPrefab;
 
     [SerializeField] private List<ItemData_Equipment_SO> craftEquipment;
-    private List<UI_CraftSlot> craftSlots = new List<UI_CraftSlot>();
-
+    private static bool isInit = false;
     // Start is called before the first frame update
     void Start()
     {
-        AssignCraftSlots();
-    }
-
-    private void AssignCraftSlots()
-    {
-        for (int i = 0; i < craftSlotParent.childCount; i++)
+        if (!isInit)
         {
-            craftSlots.Add(craftSlotParent.GetChild(i).GetComponent<UI_CraftSlot>());
+            isInit = true;
+            transform.parent.GetChild(0).gameObject.GetComponent<UI_CraftList>().SetupCraftList();//button weapon craft list
+            SetupDefaultCraftWindow();
+            Debug.Log("Init craft window");
         }
     }
 
     public void SetupCraftList()
     {
         //clear all exist
-        for (int i = 0; i < craftSlots.Count; i++)
+        for (int i = 0; i < craftSlotParent.childCount; i++)
         {
-            Destroy(craftSlots[i].gameObject);
+            Destroy(craftSlotParent.GetChild(i).gameObject);
         }
 
-        //creat new list
-        craftSlots = new List<UI_CraftSlot>();
         for (int i = 0; i < craftEquipment.Count; i++)
         {
             GameObject newSlot = Instantiate(craftSlotPrefab, craftSlotParent);
             newSlot.GetComponent<UI_CraftSlot>().SetupCraftSlot(craftEquipment[i]);
-            craftSlots.Add(newSlot.GetComponent<UI_CraftSlot>());
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         SetupCraftList();
+    }
+
+    public void SetupDefaultCraftWindow()
+    {
+        if (craftEquipment.Count > 0)
+        {
+            GetComponentInParent<UI>().craftWindow.SetupCraftWindow(craftEquipment[0]);
+        }
     }
 }
