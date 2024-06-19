fd8b411ab1c0d60c63ee15389afeb9f8ae1904e3
Thành Trần Tấn <trantanthanh@gmail.com>
2024-06-20 01:09:11 +0700

    save game when reach checkpoint

--- Assets/_Scripts/Save and Load/Checkpoint.cs
+++ Assets/_Scripts/Save and Load/Checkpoint.cs
@@ -28,6 +28,7 @@ public class Checkpoint : MonoBehaviour
             {
                 ActivateCheckpoint();
                 CheckDeactiveOthers();
+                SaveManager.Instance.SaveGame();
             }
         }
     }
