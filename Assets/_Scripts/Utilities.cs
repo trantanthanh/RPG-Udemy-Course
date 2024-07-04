58c880b870a0ae09942b3722e3b3ed3eeeb70545
Thành Trần Tấn <trantanthanh@gmail.com>
2024-07-05 01:35:39 +0700

    add more code for extension method float, use weak reference for WaitForSeconds extension

--- Assets/_Scripts/Utilities.cs
+++ Assets/_Scripts/Utilities.cs
@@ -1,20 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Utilities
 {
     public static class MyExtensions
     {
-        private static readonly Dictionary<float, WaitForSeconds> _wfsDictionary = new Dictionary<float,
-    WaitForSeconds>();
+    //    private static readonly Dictionary<float, WaitForSeconds> _wfsDictionary = new Dictionary<float,
+    //WaitForSeconds>();
+    //    public static WaitForSeconds WaitForSeconds(this float time)
+    //    {
+    //        if (_wfsDictionary.TryGetValue(time, out WaitForSeconds waitForSeconds))
+    //        {
+    //            return waitForSeconds;
+    //        }
+    //        var newWaitForSeconds = new WaitForSeconds(time);
+    //        _wfsDictionary.Add(time, newWaitForSeconds);
+    //        return newWaitForSeconds;
+    //    }
+
+        private static readonly Dictionary<float, WeakReference<WaitForSeconds>> _wfsDictionary = new Dictionary<float, WeakReference<WaitForSeconds>>();
         public static WaitForSeconds WaitForSeconds(this float time)
         {
-            if (_wfsDictionary.TryGetValue(time, out WaitForSeconds waitForSeconds))
+            if (_wfsDictionary.TryGetValue(time, out WeakReference<WaitForSeconds> weakRef))
             {
-                return waitForSeconds;
+                if (weakRef.TryGetTarget(out WaitForSeconds waitForSeconds))
+                {
+                    return waitForSeconds;
+                }
             }
+
             var newWaitForSeconds = new WaitForSeconds(time);
-            _wfsDictionary.Add(time, newWaitForSeconds);
+            _wfsDictionary[time] = new WeakReference<WaitForSeconds>(newWaitForSeconds);
             return newWaitForSeconds;
         }
     }
